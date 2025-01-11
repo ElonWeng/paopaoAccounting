e09ebbe6d127031fabe413f093fabaebcadc2949
Elon <229478140@qq.com>
2025-01-11 16:05:52 +0800

    【提交人】Elon 【提交更改】添加了bottom bar 【开发耗时】

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -4,6 +4,7 @@ import 'package:paopao_accounting/ui/main/home/HomePage.dart';
 
 import '../base/util/routes_util.dart';
 import '../base/widget/base_page.dart';
+import '../base/widget/bottom_bar.dart';
 
 class MainPage extends StatefulWidget {
 
@@ -17,18 +18,10 @@ class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
   static const List<Widget> _widgetOptions = <Widget>[
     HomePage(),
-    Center(child: Text('Search')),
     Center(child: Text('Profile')),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-      if(_selectedIndex == 2){
-        RoutesUtil.navigateToPage(context,LOGIN_PAGE,{});
-      }
-    });
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -41,24 +34,7 @@ class _MainPageState extends State<MainPage> {
           child: _widgetOptions.elementAt(_selectedIndex),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_home_outlined),
-            label: '首页',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt_outlined),
-            label: '账单',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_2_outlined),
-            label: '我的',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+      bottomNavigationBar: BottomBar()
     );
   }
 }
